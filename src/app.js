b3e7020d73d437dfbe97b760c8bf3561251800fd
Dinh Dat <datddph15467@fpt.edu.vn>
2022-03-17 14:46:25 +0700

    update

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// const express = require("express");
+import express from 'express';
+import productRouter from './routes/product';
+import categoryRouter from './routes/category';
+
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+// Routing
+app.use("/api", productRouter);
+app.use("/api", categoryRouter);
+// connect database
+mongoose.connect("mongodb://localhost:27017/we16306")
+    .then(() => console.log("Connect db thanh cong"))
+    .catch((error) => console.log(error))
+    // Connect
+const PORT = 8000;
+app.listen(PORT, () => {
+    console.log(`Server đang chạy cổng ${PORT}`);
+});
